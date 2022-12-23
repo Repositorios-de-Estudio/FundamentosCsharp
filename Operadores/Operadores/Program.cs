00aa3a5635a5f725c90d61b6f6a173bdc0263c5e
SergioPerez <dark07perez@gmail.com>
2022-12-23 02:53:16 -0500

    operadores

---
+++ Operadores/Operadores/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Operadores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /* 1
+             * Operadores ARITMETICOS o binarios*
+             * Suma, resta +, -
+             * Multiplicacion, division, *, /
+             * Modulo %
+            */
+         
+            int a = 30;
+            int b = 11;
+
+            Console.WriteLine(a + b);
+            Console.WriteLine(a * b);
+            Console.WriteLine(a % b);
+
+            /* 2
+             * Operadores unarios
+             * Incremento, decremento ++, --
+             * 
+             */
+            a++;
+            Console.WriteLine(a);
+
+
+            /* 3
+             * Operadores de comparacion -> retorna booleano
+             * Mayor que, menor que, >, <
+             * Mayor o igual que, menor o igual que, >= , <=
+             * igual, difernete, ==, !=
+             */
+
+            Console.WriteLine(a > b);
+            Console.WriteLine(a <= b);
+            Console.WriteLine(a == b);
+
+
+            int num = 19;
+            int divisor = 5;
+            int resultado = num % divisor;
+
+            Console.WriteLine(resultado);
+        }
+    }
+}
