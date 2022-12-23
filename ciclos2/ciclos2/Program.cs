e9b13e1673461b736bcc226bd3898070430d6cc8
SergioPerez <dark07perez@gmail.com>
2022-12-23 03:43:17 -0500

    ciclos 2 - break - continue

---
+++ ciclos2/ciclos2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ciclos2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+             * Ciclos 2
+             * Break: rompe ejecucion
+             * Continue: salta las linea a continuacion y continua a la siguiente iteracion
+             */
+
+            // uso de Break
+            for (int i = 0; i <10; i++)
+            {
+                Console.WriteLine("for {0}", i);
+
+                if (i == 5)
+                {
+                    Console.WriteLine("for {0} BREAK", i);
+                    break;
+                }
+            }
+
+
+            // uso de Continue
+            int a = 0;
+            while (a < 10)
+            {
+
+                // si es impar lo salta 
+                if (a%2!=0)
+                {
+                    a++;
+                    continue;
+                }
+
+
+                Console.WriteLine("While PAR {0}", a);
+                a++;
+            }
+
+
+
+        }
+    }
+}
