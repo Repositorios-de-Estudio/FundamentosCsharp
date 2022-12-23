29ca33b4fd34c9b1a2435cd4462f624737b3f62d
SergioPerez <dark07perez@gmail.com>
2022-12-23 03:53:44 -0500

    leer datos desde teclado

---
+++ leer_datos_de_teclado/leer_datos_de_teclado/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace leer_datos_de_teclado
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Leer datos desde el teclado
+             *  Parse: convetir tipos de datos
+             */
+
+
+
+            Console.WriteLine("SUMA DE DOS NUMEROS");
+
+            Console.WriteLine("Ingrese el primer numero");
+            string entrada1 = Console.ReadLine();
+            int num1 = int.Parse(entrada1);
+
+
+            Console.WriteLine("Ingrese el segundo numero");
+            string entrada2 = Console.ReadLine();
+            int num2 = int.Parse(entrada2);
+
+
+            int result = num1 + num2;
+            Console.WriteLine("Resultado suma: {0}",result );
+        }
+    }
+}
