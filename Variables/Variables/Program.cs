6a625aee10dac0f3853af5c2e1f7aa711bb9a4c5
SergioPerez <dark07perez@gmail.com>
2022-12-23 02:04:31 -0500

    variables

---
+++ Variables/Variables/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 10;
+            int resultado = numero * 5;
+
+            int suma = 8 + 20;
+
+            // las cadenas siempre son con comillas dobles
+            string cadena = "cadena - string en comillas dobles";
+
+            Console.WriteLine("resulado: {0}", resultado);
+                    
+            Console.WriteLine("suma: {0}", suma);
+            Console.WriteLine("cadena: {0}", cadena);
+
+
+
+
+        }
+    }
+}
