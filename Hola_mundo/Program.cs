82a0eee1e6b656c0dd4799027d75fdb627ef8c8c
SergioPerez <dark07perez@gmail.com>
2022-12-23 00:12:51 -0500

    hola mundo

---
+++ Hola_mundo/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+
+
+
+// See https://aka.ms/new-console-template for more information
+
+
+
+
+
+namespace HolaMundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo, yo C# console app!");
+            Console.WriteLine("Otra linea writeline");
+        }
+    }
+}
+
