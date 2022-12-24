e7524909f9e33c31894911e615c099ec7c79ad0f
SergioPerez <dark07perez@gmail.com>
2022-12-23 21:39:45 -0500

    clases
    POOB clases sin constructor

---
+++ Clases-POOB/Clases-POOB/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Clases_POOB
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // crea un objeto de tipo Rectangulo
+
+            Rectangulo rect = new Rectangulo();
+            rect.Altura = 10;
+            rect.Base = 11;
+
+            int area = rect.CalcularArea();
+
+
+            Console.WriteLine("Area 1, base {0} x altura {1} = {2} unidades cuadradas", rect.Base, rect.Altura, area);
+
+
+            // sintaxis de inicializacion
+
+            Rectangulo rect2 = new Rectangulo { Base = 3, Altura = 8 };
+            int area2 = rect2.CalcularArea();
+
+            Console.WriteLine("Area 2, base {0} x altura {1} = {2} unidades cuadradas", rect2.Base, rect2.Altura, area2);
+
+        }
+    }
+}
---
+++ Clases-POOB/Clases-POOB/Rectangulo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Clases_POOB
+{
+    internal class Rectangulo
+    {
+        public int Base { get; set; }
+        public int Altura { get; set; }
+
+        public int CalcularArea()
+        {
+            return Base * Altura;
+        }
+
+       
+    }
+}
