24a154a14c934a944079d9cf4c28206d4c438deb
SergioPerez <dark07perez@gmail.com>
2022-12-23 23:28:26 -0500

    propiedad automatica vs completa

---
+++ Propiedades/Propiedades/CuentaBancaria.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Propiedades
+{
+    public class CuentaBancaria
+    {
+        // Auto properties (propiedades automáticas)
+        public string NoCuenta { get; set; }
+        public string Usuario { get; set; }
+
+        // backing field - _saldo es variable privada solo para calculo del Saldo
+        // Se expanden los metodos set y get de Saldo en la declaracion
+        private decimal _saldo;
+
+        // Full property (propiedad completa)
+        public decimal Saldo
+        {
+            get { return _saldo; }
+            // Evitar que el saldo sea negativo, uso de operador ternario
+            set { _saldo = value < 0 ? 0 : value; }
+        }
+
+        //codigo extendido para set
+        /*
+            // Evitar que el saldo sea negativo
+            if (value< 0)
+            {
+                _saldo = 0;
+            }
+            else
+            {
+                _saldo = value;
+            }
+        */
+
+        #region constructores
+        public CuentaBancaria(string noCuenta)
+        {
+            NoCuenta = noCuenta;
+        }
+
+        public CuentaBancaria(string noCuenta, string usuario)
+            : this(noCuenta)
+        {
+            Usuario = usuario;
+        }
+
+        public CuentaBancaria(string noCuenta, string usuario, decimal saldo)
+            : this(noCuenta, usuario)
+        {
+            Saldo = saldo;
+        }
+
+        public CuentaBancaria() { }
+        #endregion
+    }
+}
