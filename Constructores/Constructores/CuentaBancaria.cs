9bd082539b3e086bb2a6d5bfa27d8559fbb57894
SergioPerez <dark07perez@gmail.com>
2022-12-23 22:12:17 -0500

    constructores

---
+++ Constructores/Constructores/CuentaBancaria.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Constructores
+{
+    internal class CuentaBancaria
+    {
+        public string Cuenta { get; set; }
+        public string Usuario { get; set; } 
+        public decimal Saldo { get; set; }
+
+        // pueden haber varios constructores
+        // se pueden llamar constructores dentro de otro costructor
+        // los constructores no tienen tipo de retorno, a diferencia de los metodos
+
+
+        public CuentaBancaria(string cuenta)
+        {
+            Cuenta = cuenta;
+        }
+
+        //usando :this(parametro, parametro) busca el constructor que corresponda y lo llama
+        public CuentaBancaria(string cuenta, string usuario): this(cuenta)
+        {
+            Usuario = usuario;
+        }
+
+        public CuentaBancaria(string cuenta, string usuario, decimal saldo) 
+        { 
+            Cuenta = cuenta; 
+            Usuario = usuario;
+            Saldo = saldo;
+        }
+
+        // se puede crear un constructor vacio
+        public CuentaBancaria()
+        {
+
+        }
+    
+    }
+}
---
+++ Constructores/Constructores/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Constructores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // clase que sea una Cuenta bancaria, 
+            // num de cuenta, id usuario, saldo
+
+            // usando constructor con un argumento
+            CuentaBancaria cuenta1 = new CuentaBancaria("0000001");
+            Console.WriteLine("Cuenta: {0}, Usuario: {1}, Saldo: {2}", 
+                cuenta1.Cuenta, cuenta1.Usuario, cuenta1.Saldo);
+
+            // usando constructor con dos argumento - este usa this()
+            CuentaBancaria cuenta2 = new CuentaBancaria("0000002", "Pepito Fulano");
+            Console.WriteLine("Cuenta: {0}, Usuario: {1}, Saldo: {2}",
+                cuenta2.Cuenta, cuenta2.Usuario, cuenta2.Saldo);
+
+            // usando constructor con tres argumentos
+            CuentaBancaria cuenta3 = new CuentaBancaria("0000003", "Firulais Rodriguez", 100012313);
+            Console.WriteLine("Cuenta: {0}, Usuario: {1}, Saldo: {2}",
+                cuenta3.Cuenta, cuenta3.Usuario, cuenta3.Saldo);
+
+        }
+    }
+}
