5e6611a7bb40e6c87bf02409d3895a2abedefdf5
SergioPerez <dark07perez@gmail.com>
2022-12-24 02:08:13 -0500

    mod acceso - private protected public internal

---
+++ Control_de_acceso/Control_de_acceso/MiClase.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OtroNamespace
+{
+    class MiClase
+    {
+
+        public void MetodoPublic()
+        {
+            // puede ser visto desde cualquier clase
+            Console.WriteLine("Metodo public");
+        }
+
+        protected void MetodoProtected()
+        {
+            // puede ser visto desde esta clase o cualquier que herede de esta
+            Console.WriteLine("Metodo protected");
+        }
+
+        internal void MetodoInternal()
+        {
+            // puede ser visto por cualquier clase dentro del mismo assembly
+            Console.WriteLine("Metodo internal");
+        }
+
+        private void MetodoPrivate()
+        {
+            // solo puede ser visto desde la propia clase
+            Console.WriteLine("Metodo private");
+        }
+
+
+    }
+}
---
+++ Control_de_acceso/Control_de_acceso/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using OtroNamespace;
+
+namespace Control_de_acceso
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //sin user using
+            //OtroNamespace.MiClase miClase= new OtroNamespace.MiClase();
+
+            MiClase miClase = new MiClase();
+            miClase.MetodoPublic();
+            //miClase.MetodoProtected();
+            miClase.MetodoInternal();
+            //miClase.MetodoPrivate();
+        }
+    }
+}
