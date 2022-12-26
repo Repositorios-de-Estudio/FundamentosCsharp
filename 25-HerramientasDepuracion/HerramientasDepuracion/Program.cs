e225591336c6445fe8ea84d655c9b076631a3790
SergioPerez <dark07perez@gmail.com>
2022-12-26 16:59:34 -0500

    organizacion

---
+++ 25-HerramientasDepuracion/HerramientasDepuracion/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HerramientasDepuracion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = { 10, 5, 9, -5, 7 };
+            int resultado = SumaElementos(numeros);
+
+            Console.WriteLine("La suma es {0}", resultado);
+        }
+
+        static int SumaElementos(int[] arreglo)
+        {
+            int suma = 0;
+            for (int i = 0; i < arreglo.Length; i++)
+            {
+                suma += arreglo[i];
+            }
+
+            return suma;
+        }
+    }
+}
