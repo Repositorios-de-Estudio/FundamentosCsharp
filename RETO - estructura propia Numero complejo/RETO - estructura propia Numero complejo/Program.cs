9abf226a18fdd9877b749ddfe9aefe2a086ab13f
SergioPerez <dark07perez@gmail.com>
2022-12-27 00:12:20 -0500

    Update Program.cs

--- RETO - estructura propia Numero complejo/RETO - estructura propia Numero complejo/Program.cs
+++ RETO - estructura propia Numero complejo/RETO - estructura propia Numero complejo/Program.cs
@@ -13,7 +13,17 @@ namespace RETO___estructura_propia_Numero_complejo
             NComplejo n1 = new NComplejo(3,2);
             NComplejo n2 = new NComplejo(5,6);
 
-            Console.WriteLine(n1.Multiplicacion(n2));
+            Console.WriteLine("Sum: "+n1.Suma(n2));
+            Console.WriteLine("Mul: "+n1.Multiplicacion(n2));
+
+            Console.WriteLine("-------------------------------");
+
+
+            n1 = new NComplejo(3, 4);
+            n2 = new NComplejo(-0.5, 2);
+
+            Console.WriteLine("Sum: "+n1.Suma(n2));
+            Console.WriteLine("Mul: "+n1.Multiplicacion(n2));
         }
     }
 }
