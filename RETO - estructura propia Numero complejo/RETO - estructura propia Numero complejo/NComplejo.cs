d18bf28c87c853cafecaeb0f38003203303fe87a
SergioPerez <dark07perez@gmail.com>
2022-12-27 00:01:58 -0500

    RETO - numeros complejos
    suma y multiplicacion

---
+++ RETO - estructura propia Numero complejo/RETO - estructura propia Numero complejo/NComplejo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RETO___estructura_propia_Numero_complejo
+{
+    internal struct NComplejo
+    {
+        public double Real { get; }
+        public double Imaginario { get; }
+
+        public NComplejo(double real, double imaginario): this()
+        {
+            Real = real;
+            Imaginario = imaginario;
+        }
+
+        public NComplejo Suma(NComplejo otro)
+        {
+            return new NComplejo(Real+otro.Real, Imaginario+otro.Imaginario);
+        }
+
+        public NComplejo Multiplicacion(NComplejo otro)
+        {
+            return new NComplejo(Real*otro.Real+Imaginario*otro.Imaginario*-1,Real*otro.Imaginario+Imaginario*otro.Real);
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Num compejo: {0}+{1}i", Real, Imaginario);
+        }
+
+    }
+}
---
+++ RETO - estructura propia Numero complejo/RETO - estructura propia Numero complejo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RETO___estructura_propia_Numero_complejo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            NComplejo n1 = new NComplejo(3,2);
+            NComplejo n2 = new NComplejo(5,6);
+
+            Console.WriteLine(n1.Multiplicacion(n2));
+        }
+    }
+}
